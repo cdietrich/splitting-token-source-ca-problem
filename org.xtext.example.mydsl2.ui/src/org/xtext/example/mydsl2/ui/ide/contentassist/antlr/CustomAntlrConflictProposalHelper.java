7867f4179db725e6d257f8223c38adc6bfa9d2fa
Christian Dietrich <christian.dietrich@itemis.de>
2023-02-08 10:43:42 +0100

    better version of conflict helper
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.xtext.example.mydsl2.ui/src/org/xtext/example/mydsl2/ui/ide/contentassist/antlr/CustomAntlrConflictProposalHelper.java
+++ org.xtext.example.mydsl2.ui/src/org/xtext/example/mydsl2/ui/ide/contentassist/antlr/CustomAntlrConflictProposalHelper.java
@@ -2,16 +2,39 @@ package org.xtext.example.mydsl2.ui.ide.contentassist.antlr;
 
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
+import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenSource;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.parser.antlr.Lexer;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.antlr.AntlrProposalConflictHelper;
 import org.xtext.example.mydsl2.parser.antlr.MyDslSplittingTokenSource;
+import org.xtext.example.mydsl2.services.MyDslGrammarAccess;
+
+import com.google.inject.Inject;
 
 /**
  *
  */
 public class CustomAntlrConflictProposalHelper extends AntlrProposalConflictHelper {
+	
+	@Inject
+	private MyDslGrammarAccess ga;
+	
+	@Override
+	public boolean existsConflict(String proposal, ContentAssistContext context) {
+		for (EObject o : context.getFirstSetGrammarElements()) {
+			if (o instanceof RuleCall) {
+				if (o == ga.getUseAccess().getDefRefsDefRefParserRuleCall_2_0()) {
+					//return false;
+				}
+			}
+		}
+		return super.existsConflict(proposal, context);
+		
+	}
+	
 
     @Override
     protected void initTokenSource(String text, TokenSource tokenSource, ContentAssistContext context) {
@@ -31,20 +54,51 @@ public class CustomAntlrConflictProposalHelper extends AntlrProposalConflictHelp
     	System.err.println("f:" + first + " s: "+ second);
     	return super.equalTokenSequence(first, second);
     }
-
+    
+//    TokenSource pl;
+//    @Override
+//    public TokenSource getProposalLexer() {
+//    	if (pl == null) {
+//    		pl = new MyDslSplittingTokenSource(super.getProposalLexer());
+//    	}
+//        return pl ;
+//    }
+//    
+//    TokenSource cl;
+//
+//    @Override
+//    public TokenSource getCombinedLexer() {
+//    	if (cl == null) {
+//    		cl = new MyDslSplittingTokenSource(super.getCombinedLexer());
+//    	}
+//        return cl;
+//    }
+//    
+//    TokenSource lcl;
+//
+//    @Override
+//    public TokenSource getLastCompleteLexer() {
+//    	if (lcl == null) {
+//    		lcl = new MyDslSplittingTokenSource(super.getLastCompleteLexer());
+//    	}
+//        return lcl;
+//    }
+    
     @Override
-    public TokenSource getProposalLexer() {
-        return new MyDslSplittingTokenSource(super.getProposalLexer());
-    }
-
-    @Override
-    public TokenSource getCombinedLexer() {
-        return new MyDslSplittingTokenSource(super.getCombinedLexer());
-    }
-
-    @Override
-    public TokenSource getLastCompleteLexer() {
-        return new MyDslSplittingTokenSource(super.getLastCompleteLexer());
-    }
+  	public boolean existsConflict(String lastCompleteText, String proposal, ContentAssistContext context) {
+  		initTokenSources(lastCompleteText, proposal, context);
+  		TokenSource lastCompleteLexer = new MyDslSplittingTokenSource(getLastCompleteLexer());
+  		TokenSource combinedLexer = new MyDslSplittingTokenSource(getCombinedLexer());
+  		TokenSource proposalLexer = new MyDslSplittingTokenSource(getProposalLexer());
+  		
+  		if (!equalTokenSequence(lastCompleteLexer, combinedLexer))
+  			return true;
+  		if (!equalTokenSequence(proposalLexer, combinedLexer))
+  			return true;
+  		Token lastToken = proposalLexer.nextToken();
+  		if (!lastToken.equals(Token.EOF_TOKEN))
+  			return true;
+  		return false;
+  	}
 
 }
